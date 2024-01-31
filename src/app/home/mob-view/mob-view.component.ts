b4bdcd40b75134227b882ef808e0cf8c408b9cae
M Hamza Siddiqui <hamza@technomites.com>
2024-01-31 22:12:50 +0500

    ...

--- src/app/home/mob-view/mob-view.component.ts
+++ src/app/home/mob-view/mob-view.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, DatePipe, isPlatformBrowser } from '@angular/common';
-import { Component, HostBinding, OnInit, effect, signal, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, HostBinding, OnInit, effect, signal, Inject, PLATFORM_ID, HostListener } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
@@ -16,14 +16,39 @@ import { RouterModule } from '@angular/router';
   templateUrl: './mob-view.component.html',
   styleUrl: './mob-view.component.scss'
 })
-export class MobViewComponent {
+export class MobViewComponent implements OnInit {
 
   contactForm:FormGroup | any;
   currentTime: Date = new Date();
+  pageChange:string = 'hero';
+  headerCount: number = 1;
+  headerToggle: boolean = false;
   darkModeValue:any;
   darkMode = signal<any>(this.darkValue);
   @HostBinding('class.dark') get mode(){
     return this.darkMode();
+  }
+
+  @HostListener('window:scroll', ['$event']) onscroll() {
+    const scrollY = window.scrollY;
+
+    if(!this.headerToggle){
+      if(scrollY > 400 && scrollY < 1100){
+        this.pageChange ='about';
+        console.warn(this.pageChange)
+      }
+      else if (scrollY > 1100 && scrollY < 1750){
+        this.pageChange ='projects';
+      }
+      else if (scrollY > 1750){
+        this.pageChange ='contact';
+      }
+      else {
+        this.pageChange = 'hero'
+      }
+    }
+    
+    
   }
 
   constructor(private _fb:FormBuilder, @Inject(PLATFORM_ID) private platformId: object){
@@ -34,9 +59,16 @@ export class MobViewComponent {
         setTimeout(() => {
           this.darkModeValue = localStorage.getItem('dark');  // For toggle button, the boolean is string acctually
         }, 100);
+
+        
       }
       
     });
+  }
+  ngOnInit(): void {
+    // if (isPlatformBrowser(this.platformId)){
+    //   scroll(0,0)
+    // }
   }
 
   get darkValue() {  // Returning the boolean value for signal variable
@@ -62,7 +94,8 @@ export class MobViewComponent {
   }
 
   toggle(){
-
+    this.headerCount++;
+    this.headerCount % 2 == 0 ? this.headerToggle = true : this.headerToggle = false;
   }
 
 }
